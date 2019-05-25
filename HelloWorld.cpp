abe1dc722c8f3966c2bca6b23e91aa97176d2ae6
Joel Lim <103879u@gmail.com>
2019-05-26 01:50:59 +0800

    reorganize

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,11 +2,19 @@
 
 using namespace std;
 
-int main() {
+void e2(){
   cout << "Print the sum of two numbers : ";
   int x, y, sum;
   cin >> x;
   cin >> y;
   sum = x + y;
   cout << "The sum of " << x << " and " << y << " is : " << sum;
+}
+
+void e3() {
+
+}
+
+int main() {
+  e2();
 }
