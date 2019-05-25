422f17ab4c03f6d84b5b4c718c5bd5cfaddaf2d6
Joel Lim <103879u@gmail.com>
2019-05-26 01:49:57 +0800

    exercise 2

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main() {
-  printf("Hello World\n");
-  cout << "two\nthree";
+  cout << "Print the sum of two numbers : ";
+  int x, y, sum;
+  cin >> x;
+  cin >> y;
+  sum = x + y;
+  cout << "The sum of " << x << " and " << y << " is : " << sum;
 }
